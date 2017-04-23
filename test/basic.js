6cbe69e309932aa793aad2a76a6ce8a7af9929c3
BAAMOSK <bunth667@gmail.com>
2017-04-23 14:12:30 -0400

    fixed some stuff

--- test/basic.js
+++ test/basic.js
@@ -1,7 +1,7 @@
-let mocha = require("mocha");
+let mocha = require('mocha');
 
-describe("basic", function() {
-  it("is it true", function() {
+describe('basic', function() {
+  it('is it true', function() {
     return 1 === 1;
   }); 
 });
